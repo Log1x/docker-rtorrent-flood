56aa320e8735fc5ead79c5d723f3d239afd7fcc6
Brandon <brandon@tendency.me>
2019-10-18 00:37:48 -0500

    Bump

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -4,6 +4,6 @@ export TERM="xterm"
 
 trap "kill $PID" HUP INT TERM
 su-exec $PUID:$PGID npm --prefix /usr/local/flood run start &
-su-exec $PUID:$PGID rtorrent -n -o import=/config/rtorrent.rc "$@" &
+su-exec $PUID:$PGID rtorrent -n -o import=/config/rtorrent.rc "$@"
 PID=$!
 wait $PID
